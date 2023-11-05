059da856bcdac5e9666f6fe9f9a2a88d3d539ead
Nike <koliaso@gmail.com>
2023-11-05 17:53:47 +0200

    loader

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,55 +1,51 @@
-import React, { useState, useEffect } from 'react';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Searchbar from './Searchbar/Searchbar';
-import ImageGallery from './ImageGallery/ImageGallery';
-import fetchImagesWithQuery from 'services/api';
-import Modal from './Modal/Modal';
-import Loader from './Loader/Loader';
-import Button from './Button/Button';
-import s from './App.module.css';
+import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Searchbar from "./Searchbar/Searchbar";
+import ImageGallery from "./ImageGallery/ImageGallery";
+import fetchImagesWithQuery from "../services/api";
+import Modal from "./Modal/Modal";
+import Loader from "./Loader/Loader";
+import Button from "./Button/Button";
+import s from "./App.module.css";
 
 export default function App() {
-  const [searchData, setSearchData] = useState('');
+  const [searchData, setSearchData] = useState("");
   const [images, setImages] = useState([]);
-  const [page, setPage] = useState(0);
-  const [largeImage, setLargeImage] = useState('');
+  const [page, setPage] = useState(1);
+  const [largeImage, setLargeImage] = useState("");
   const [showModal, setShowModal] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    if (!page) {
-      return;
-    }
-
     const fetchData = async () => {
       setIsLoading(true);
       try {
         const response = await fetchImagesWithQuery(searchData, page);
         const data = response.data;
         if (data.hits.length === 0) {
-          toast.error('Nothing found');
+          toast.error("Nothing found");
         } else {
-          const newImages = data.hits.filter(
-            ({ id }) => !images.some((image) => image.id === id)
-          );
-          setImages((prevImages) => [...prevImages, ...newImages]);
+          setImages((prevImages) => {
+            const newImages = data.hits.filter(({ id }) => !prevImages.some((image) => image.id === id));
+            return [...prevImages, ...newImages];
+          });
         }
       } catch (error) {
-        console.error(error); 
+        console.error(error);
       } finally {
         setIsLoading(false);
       }
     };
 
-    fetchData();
-  }, [page, searchData, images]);
+    if (searchData !== "") {
+      fetchData();
+    }
+  }, [searchData, page]);
 
   const onSubmit = (newSearchData) => {
-    if (newSearchData.trim() === '') {
-      return toast.error('Enter a search term');
-    } else if (newSearchData === searchData) {
-      return;
+    if (newSearchData.trim() === "") {
+      return toast.error("Enter a search term");
     }
     setSearchData(newSearchData);
     setPage(1);
